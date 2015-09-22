0fa67a45a7c674f3bbc96347689beb4b4de046fc
usuario <usuario@10.22.143.11>
2015-09-22 09:58:20 +0100

    practica uno con metodo

--- src/practica/is/Person.java
+++ src/practica/is/Person.java
@@ -14,6 +14,7 @@ public class Person {
     private final String name;
     private final String surname;
     private final Date cumple;
+    private static final long MILLISECONDS_PER_YEAR = (long) (1000*60*60*24*365.25);
 
     public Person(String name, String surname, Date cumple) {
         this.name = name;
@@ -37,6 +38,10 @@ public class Person {
     }
     public int getAge(){
         Date today = new Date();
-        return (int) ((today.getTime() - cumple.getTime()) / (1000*60*60*24*365.25));
+        return (int)miMetodo(today.getTime()-cumple.getTime());
+    }
+    
+    private long miMetodo (long millis){
+        return millis / MILLISECONDS_PER_YEAR;
     }
 }
